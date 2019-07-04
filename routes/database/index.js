ed4950c6d72f6577e00081524b01ecafa5dbba1b
jivinjules <juliehodges05@gmail.com>
2019-07-03 22:36:30 -0500

    test 4

--- routes/database/index.js
+++ routes/database/index.js
@@ -5,8 +5,10 @@ mongoose.Promise = global.Promise
 //your local database url
 //27017 is the default mongoDB port
 // const uri = 'mongodb://localhost:27017/simple-mern-passport' 
+const uri = 'mongodb://resu:resuuser2019@ds345587.mlab.com:45587/heroku_tfm9bttk'
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/passport").then(
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/passport").then(
+    mongoose.connect(uri).then(
     () => { 
         /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ 
         console.log('Connected to Mongo');
